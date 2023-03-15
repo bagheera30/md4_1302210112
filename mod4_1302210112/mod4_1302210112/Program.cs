c76d0de7c8975068c9c0df7d1ed1b0c34baa534b
bagheera30 <r.boeran@gmail.com>
2023-03-15 16:58:02 +0700

    menambahkan tabel buah

--- mod4_1302210112/mod4_1302210112/Program.cs
+++ mod4_1302210112/mod4_1302210112/Program.cs
@@ -1,2 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+
+internal class Program
+{
+    public enum buah
+    {
+        apel,
+        aprikot,
+        alpukat,
+        pisang,
+        paprika,
+        blackberry,
+        ceri,
+        kelapa,
+        jagung
+    }
+    public static string getKode(buah b)
+    {
+        string[] kdbuah =
+        {
+            "A00","C00","C00","d00","E00","F00","H00","I00","J00"
+        };
+        return kdbuah[(int)b];
+    }
+
+    private static void Main(string[] args)
+    {
+        
+        buah b=buah.apel;
+        Console.WriteLine("buah" + b + "kode buah" + getKode(b);
+    }
+}
